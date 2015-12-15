bca03d8d653bca9cbffcc340a510457184607dac
zhoutk <zhoutk@189.cn>
2015-12-15 13:06:27 +0800

    class image

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var jsdom = require("jsdom");
+var cheer = require("cheerio");
 var fs = require("fs");
-var jquery = fs.readFileSync("./jquery.js", "utf-8");
+// var jquery = fs.readFileSync("./jquery.js", "utf-8");
+var data = require('promised-rest-client')({url: 'http:'});
+var Promise = require('promise');
 
-jsdom.env({
-  url: "https://en.wikipedia.org/wiki/Aircraft_carrier",
-  src: [jquery],
-  done: function (err, window) {
-    var $ = window.$;
-    $("html").each(function () {
-      var detail = $(this).html();
+var downHtml = fs.readFileSync('./carrier.txt');
 
-    });
-  }
-});
+$ = cheer.load(downHtml);
+
+var imgs = $('.image');
+var i = 0;
+for(img in imgs){
+  if(typeof imgs[img].attribs === 'undefined' || typeof imgs[img].attribs.href === 'undefined')
+    { continue;}
+  else
+    {
+      
+      console.log(i++ +"--"+imgs[img].children[0].attribs.src);
+      break;
+    }
+}
