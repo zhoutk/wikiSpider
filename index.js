c0097543a781ab0f61e57e02599f830220cfac3a
zhoutk <zhoutk@189.cn>
2015-12-16 21:50:20 +0800

    url给定方式调整为直接取href。

--- index.js
+++ index.js
@@ -2,34 +2,38 @@ var cheer = require('cheerio');
 var Promise = require('promise');
 var data = require('promised-rest-client')({url: 'https://zh.wikipedia.org/zh-cn/'});
 var spiderSingle = require('./spiderSingle.js');
-var allKeys = ['航空母舰'];
-var keys = ['航空母舰'];
+var regKey = ['航空母舰','航空母艦'];
+var allKeys = [];
+var keys = ['Category:%E8%88%AA%E7%A9%BA%E6%AF%8D%E8%88%B0'];
 
 var key = keys.shift();
 (function doNext(key){
 
   data.get({
-    url:encodeURI(key),
+    url:key,
     qs:null
   }).then(function(downHtml){
-    console.log("down:"+key+"..."+keys.length);
+    console.log('开始处理：'+decodeURI(key)+'，队列中还有'+keys.length+'个未处理。')
     $ = cheer.load(downHtml);
     var links = $('#bodyContent a');
     for(x in links){
-      var title = undefined;
-      if(typeof links[x].attribs !== 'undefined' && links[x].attribs.title !== 'undefined'){
+      var title, href;
+      if(typeof links[x].attribs !== 'undefined' && links[x].attribs.title !== 'undefined' && links[x].attribs.href !== 'undefined'){
         title = links[x].attribs.title;
-        var reg = /[:|：|(|)（|）]/;
-        if(reg.test(title))
-          continue;
-        if(title && title.indexOf(key)>-1 && !existsInKeys(title)){
-          keys.push(title);
+        href = links[x].attribs.href;
+        // var reg = /[:|：|(|)（|）]/;
+        // if(reg.test(title))
+        //   continue;
+        if(title && href && includeKeys(title) && !existsInKeys(title)){
+          href = href.split('/')[href.split('/').length-1];
+          keys.push(href);
           allKeys.push(title);
-          console.log(title)
+          //console.log(title)
         }
       }
     }
-    spiderSingle(downHtml);
+    if(!(/[:|：]/.test(key)))
+      spiderSingle(downHtml);
     key = keys.shift();
     if(key){
       doNext(key);
@@ -37,12 +41,12 @@ var key = keys.shift();
       console.log('抓取任务顺利完成。')
     }
   }).catch(function(err){
-    console.error("download err:"+key);
+    console.error("download ------- err ------- "+decodeURI(key));
     key = keys.shift();
     if(key){
       doNext(key);
     }else{
-      console.log('抓取任务顺利完成。')
+      console.log('抓取任务顺利完成，共处理了'+allKeys.length+'个页面。')
     }
   })
 
@@ -51,6 +55,15 @@ var key = keys.shift();
 function existsInKeys(key){
   for(var i=0;i<allKeys.length;i++){
     if(allKeys[i]==key){
+      return true;
+    }
+  }
+  return false;
+}
+
+function includeKeys(key){
+  for(var i=0;i<regKey.length;i++){
+    if(key.indexOf(regKey[i])>-1){
       return true;
     }
   }
